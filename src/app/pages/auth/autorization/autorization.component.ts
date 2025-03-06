6cda01b47979f4d33558ae7424632449d9cf97e8
Roman <KurochkinRV@list.ru>
2025-03-06 21:25:01 +0300

    Home Work 1

--- src/app/pages/auth/autorization/autorization.component.ts
+++ src/app/pages/auth/autorization/autorization.component.ts
@@ -1,12 +1,22 @@
+import { NgClass } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-autorization',
-  imports: [],
+  imports: [NgClass, FormsModule, ButtonModule, InputTextModule],
   templateUrl: './autorization.component.html',
   styleUrl: './autorization.component.scss',
 })
 export class AutorizationComponent implements OnInit, OnDestroy  {
+  
+  login: string;
+  password: string;
+
   ngOnInit(): void {}
   ngOnDestroy(): void {}
+
+
 }
