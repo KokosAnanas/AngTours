2c3840ca03b980fda2a36f6d5a5cde5f010d040e
Roman <KurochkinRV@list.ru>
2025-03-19 16:16:30 +0300

    Create Component Tour-Item, Class Activated Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,16 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { LayautComponent } from './layaut/layaut.component';
 import { ToursComponent } from './pages/tours/tours.component';
+import { TourItemComponent } from './pages/tour-item/tour-item.component';
 
 export const routes: Routes = [
 	{ path: 'auth', component: AuthComponent },
 	{ path: '', redirectTo: '/auth', pathMatch: 'full' },
 	{ path: 'tours', component: LayautComponent,
 		children: [
-			{ path: '', component: ToursComponent }
+			{ path: '', component: ToursComponent },
+			{ path: 'tour/:id', component: TourItemComponent },
+			{ path: 'tour', redirectTo: '', pathMatch: 'full' },
 		]
 	},
 	
--- src/app/pages/tours/tours.component.ts
+++ src/app/pages/tours/tours.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToursService } from '../../services/tours.service';
 import { CardModule } from 'primeng/card';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tours',
@@ -11,13 +12,20 @@ import { CardModule } from 'primeng/card';
 export class ToursComponent implements OnInit {
   tours: any = []; // TODO add types
 
-  constructor(private toursService: ToursService) {}
+  constructor( private toursService: ToursService,
+    private route: ActivatedRoute,
+    private router: Router ) {}
 
   ngOnInit(): void {
+    console.log('activetedRoute', this.route)
     this.toursService.getTours().subscribe((data) => {
       if (Array.isArray(data?.tours)) {
         this.tours = data.tours;
       }
     });
+  }
+
+  goToTour(item: any): void { // TODO add interface
+    this.router.navigate(['tour', item.id], {relativeTo: this.route});
   }
 }
