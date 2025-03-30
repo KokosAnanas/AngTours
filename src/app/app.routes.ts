5147ec81cd9d07203d1a5b6d505b6f4829455682
Roman <KurochkinRV@list.ru>
2025-03-31 00:15:25 +0300

    Add Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,20 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { LayautComponent } from './layaut/layaut.component';
 import { ToursComponent } from './pages/tours/tours.component';
 import { TourItemComponent } from './pages/tour-item/tour-item.component';
+import {authGuard} from './shared/guards/auth.guard';
 
 export const routes: Routes = [
 	{ path: 'auth', component: AuthComponent },
 	{ path: '', redirectTo: '/auth', pathMatch: 'full' },
-	{ path: 'tours', component: LayautComponent,
+	{ path: 'tours',
+    canActivate: [authGuard],
+    component: LayautComponent,
 		children: [
 			{ path: '', component: ToursComponent },
 			{ path: 'tour/:id', component: TourItemComponent },
 			{ path: 'tour', redirectTo: '', pathMatch: 'full' },
 		]
 	},
-	
-    { path: '**', redirectTo: '/auth', pathMatch: 'full' },  
+
+    { path: '**', redirectTo: '/auth', pathMatch: 'full' },
 ];
