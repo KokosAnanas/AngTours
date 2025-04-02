4a53825747a598694b4b128cb6c5642287970fcb
Roman <KurochkinRV@list.ru>
2025-04-02 11:37:05 +0300

    RouterLink @If ng-template

--- src/app/pages/tour-item/tour-item.component.ts
+++ src/app/pages/tour-item/tour-item.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ToursService } from '../../services/tours.service';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {Button} from 'primeng/button';
+import {Card} from 'primeng/card';
+import {PrimeTemplate} from 'primeng/api';
+import {ITour} from '../../models/tours';
 
 @Component({
   selector: 'app-tour-item',
-  imports: [],
+  imports: [ Button, RouterLink, Card, PrimeTemplate ],
   templateUrl: './tour-item.component.html',
   styleUrl: './tour-item.component.scss',
 })
 export class TourItemComponent implements OnInit {
   tourId: string = null;
-  
+  tour: ITour;
+
   constructor (private tourService: ToursService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.tourId = this.route.snapshot.paramMap.get('id');
+    console.log('TourId', this.tourId);
+    this.tourService.getTourById(this.tourId).subscribe(tour => {
+      this.tour = tour;
+    })
   }
 }
 
